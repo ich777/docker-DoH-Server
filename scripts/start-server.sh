7ce9c87a8f5b8ec79e8680a1897d876ab5cbca4f
ich777 <ich777@noreply.example.org>
2019-10-07 19:10:20 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -82,7 +82,7 @@ if [ "${DoH_V}" != $CUR_V ]; then
 	cd ${DATA_DIR}
 	rm -R ${DATA_DIR}/dns-over-https-${DoH_V} ${DATA_DIR}/go ${DATA_DIR}/gopath
 elif [ "${DoH_V" == "CUR_V" ]; then
-	echo "---Versions match! Installed: v$CUR_V | Preffered: v${DoH_V}---"
+	echo "---Versions match! Installed: v$CUR_V | Preferred: v${DoH_V}---"
 fi
 
 echo "---Preparing Server---"
