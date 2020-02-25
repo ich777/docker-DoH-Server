6cfa44c23c311497479788f0d279adc47934bc1b
ich777 <ich777@noreply.example.org>
2020-02-25 13:26:17 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,78 +6,87 @@ if [ "${DoH_V}" == "latest" ]; then
 fi
 
 echo "---Checking if DoH-Server is installed---"
-if [ ! -f ${DATA_DIR}/doh-server/doh-server ]; then
-	echo "---DoH-Server not installed, installing---"
-    cd ${DATA_DIR}
-    if wget ${GO_DL_URL} ; then
-		echo "---Sucessfully downloaded Golang---"
-    else
-		echo "---Something went wrong, can't download Golang, putting server in sleep mode---"
-		sleep infinity
-    fi
-    tar xzf go*
-    export GOROOT=/DoH/go
-    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    if [ ! -d ${DATA_DIR}/gopath ]; then
-    	mkdir gopath
-    fi
-    export GOPATH=/DoH/gopath/
-	if wget https://github.com/m13253/dns-over-https/archive/v${DoH_V}.tar.gz ; then
-    	echo "---Sucessfully downloaded DoH---"
-    else
-    	echo "---Something went wrong, can't download DoH, putting server in sleep mode---"
-        sleep infinity
-    fi
-	tar xzf v${DoH_V}.tar.gz
-	touch dohinstalled-${DoH_V}
-    CUR_V=${DoH_V}
-	rm *.tar.gz
-	cd ${DATA_DIR}/dns-over-https-${DoH_V}
-	make
-	mv ${DATA_DIR}/dns-over-https-${DoH_V}/doh-server/ ${DATA_DIR}
-	rm ${DATA_DIR}/doh-server/doh-server.conf
-	cd ${DATA_DIR}
-	rm -R ${DATA_DIR}/dns-over-https-${DoH_V} ${DATA_DIR}/go ${DATA_DIR}/gopath
+if [ ! -z "$LAT_V" ]; then
+	if [ ! -f ${DATA_DIR}/doh-server/doh-server ]; then
+		echo "---DoH-Server not installed, installing---"
+	    cd ${DATA_DIR}
+	    if wget ${GO_DL_URL} ; then
+			echo "---Sucessfully downloaded Golang---"
+	    else
+			echo "---Something went wrong, can't download Golang, putting server in sleep mode---"
+			sleep infinity
+	    fi
+	    tar xzf go*
+	    export GOROOT=/DoH/go
+	    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+	    if [ ! -d ${DATA_DIR}/gopath ]; then
+	    	mkdir gopath
+	    fi
+	    export GOPATH=/DoH/gopath/
+		if wget https://github.com/m13253/dns-over-https/archive/v${DoH_V}.tar.gz ; then
+	    	echo "---Sucessfully downloaded DoH---"
+	    else
+	    	echo "---Something went wrong, can't download DoH, putting server in sleep mode---"
+	        sleep infinity
+	    fi
+		tar xzf v${DoH_V}.tar.gz
+		touch dohinstalled-${DoH_V}
+	    CUR_V=${DoH_V}
+		rm *.tar.gz
+		cd ${DATA_DIR}/dns-over-https-${DoH_V}
+		make
+		mv ${DATA_DIR}/dns-over-https-${DoH_V}/doh-server/ ${DATA_DIR}
+		rm ${DATA_DIR}/doh-server/doh-server.conf
+		cd ${DATA_DIR}
+		rm -R ${DATA_DIR}/dns-over-https-${DoH_V} ${DATA_DIR}/go ${DATA_DIR}/gopath
+	else
+		echo "---DoH-Server found!---"
+	fi
 else
-	echo "---DoH-Server found!---"
+	echo "---Can't get latest version, putting server into sleep mode---"
+    sleep infinity
 fi
 
 echo "---Version Check---"
-if [ "${DoH_V}" != "$CUR_V" ]; then
-	echo "---Version missmatch v${CUR_V} installed, installing v${DoH_V}---"
-    rm ${DATA_DIR}/dohinstalled-${CUR_V}
-	cd ${DATA_DIR}
-    if wget ${GO_DL_URL} ; then
-		echo "---Sucessfully downloaded Golang---"
-    else
-		echo "---Something went wrong, can't download Golang, putting server in sleep mode---"
-		sleep infinity
-    fi
-    tar xzf go*
-    export GOROOT=/DoH/go
-    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    if [ ! -d ${DATA_DIR}/gopath ]; then
-    	mkdir gopath
-    fi
-    export GOPATH=/DoH/gopath/
-	if wget https://github.com/m13253/dns-over-https/archive/v${DoH_V}.tar.gz ; then
-    	echo "---Sucessfully downloaded DoH---"
-    else
-    	echo "---Something went wrong, can't download DoH, putting server in sleep mode---"
-        sleep infinity
-    fi
-	tar xzf v${DoH_V}.tar.gz
-	touch dohinstalled-${DoH_V}
-	rm *.tar.gz
-	cd ${DATA_DIR}/dns-over-https-${DoH_V}
-	make
-    rm -R ${DATA_DIR}/doh-server
-	mv ${DATA_DIR}/dns-over-https-${DoH_V}/doh-server/ ${DATA_DIR}
-	rm ${DATA_DIR}/doh-server/doh-server.conf
-	cd ${DATA_DIR}
-	rm -R ${DATA_DIR}/dns-over-https-${DoH_V} ${DATA_DIR}/go ${DATA_DIR}/gopath
-elif [ "${DoH_V}" == "$CUR_V" ]; then
-	echo "---Versions match! Installed: v$CUR_V | Preferred: v${DoH_V}---"
+if [ ! -z "$LAT_V" ]; then
+	if [ "${DoH_V}" != "$CUR_V" ]; then
+		echo "---Version missmatch v${CUR_V} installed, installing v${DoH_V}---"
+	    rm ${DATA_DIR}/dohinstalled-${CUR_V}
+		cd ${DATA_DIR}
+	    if wget ${GO_DL_URL} ; then
+			echo "---Sucessfully downloaded Golang---"
+	    else
+			echo "---Something went wrong, can't download Golang, putting server in sleep mode---"
+			sleep infinity
+	    fi
+	    tar xzf go*
+	    export GOROOT=/DoH/go
+	    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+	    if [ ! -d ${DATA_DIR}/gopath ]; then
+	    	mkdir gopath
+	    fi
+	    export GOPATH=/DoH/gopath/
+		if wget https://github.com/m13253/dns-over-https/archive/v${DoH_V}.tar.gz ; then
+	    	echo "---Sucessfully downloaded DoH---"
+	    else
+	    	echo "---Something went wrong, can't download DoH, putting server in sleep mode---"
+	        sleep infinity
+	    fi
+		tar xzf v${DoH_V}.tar.gz
+		touch dohinstalled-${DoH_V}
+		rm *.tar.gz
+		cd ${DATA_DIR}/dns-over-https-${DoH_V}
+		make
+	    rm -R ${DATA_DIR}/doh-server
+		mv ${DATA_DIR}/dns-over-https-${DoH_V}/doh-server/ ${DATA_DIR}
+		rm ${DATA_DIR}/doh-server/doh-server.conf
+		cd ${DATA_DIR}
+		rm -R ${DATA_DIR}/dns-over-https-${DoH_V} ${DATA_DIR}/go ${DATA_DIR}/gopath
+	elif [ "${DoH_V}" == "$CUR_V" ]; then
+		echo "---Versions match! Installed: v$CUR_V | Preferred: v${DoH_V}---"
+	fi
+else
+	echo "---Can't get latest version, continuing---"
 fi
 
 if [ "$(echo ${DoH_V} | sed -e 's/\.//g')" -ge "220" ]; then
